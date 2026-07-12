44c1d09aff0b7951edf006cb4c4f9ebbe7988574
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-07-12 14:19:37 +0200

    add data

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import express, { request, response } from "express";
+import express from "express";
+import type { Express, Request, Response } from "express";
+import { pets } from "./data/pets";
 
 const PORT = 8000;
 
-const app = express();
+const app: Express = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send({});
+app.get("/", (req: Request, res: Response): void => {
+  res.status(200).json(pets);
 });
 
 app.listen(PORT, (): void => {
