16401372fa801b7c4339be94c4f04c7cce719f41
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-07-17 17:18:54 +0200

    update routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 import express from "express";
+import { petRouter } from "./routes/pets.routes";
 import type { Express, Request, Response, NextFunction } from "express";
-import type { Pet } from "./data/pets";
 import cors from "cors";
-
-import { pets } from "./data/pets";
 
 const PORT = 8000;
 
@@ -11,36 +9,7 @@ const app: Express = express();
 
 app.use(cors());
 
-app.get("/", (req: Request, res: Response<Pet[]>): void => {
-  const query = req.query;
-
-  console.log(query.species);
-
-  // const filteredPets = pets.filter((pet) => {
-  //   return pet.species.toLowerCase() === query.species.toLowerCase();
-  // });
-
-  res.status(200).json(pets);
-});
-
-app.get(
-  "/pets/:id",
-  (
-    req: Request<{ id: string }>,
-    res: Response<Pet | { statusCode: number; message: string }>,
-  ): void => {
-    const id = req.params.id;
-
-    const pet: Pet | undefined = pets.find(
-      (pet: Pet): boolean => pet.id.toString() === id,
-    );
-    if (!pet) {
-      res.status(404).json({ statusCode: 404, message: "Pet not found" });
-    }
-
-    res.status(200).json(pet);
-  },
-);
+app.use("/api", petRouter);
 
 app.use(
   (
