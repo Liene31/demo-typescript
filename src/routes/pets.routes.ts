2840e4f188b3e158edfff5b5f2cfd0449aec0b0c
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-07-19 14:19:57 +0200

    add middleware

--- src/routes/pets.routes.ts
+++ src/routes/pets.routes.ts
@@ -1,74 +1,11 @@
 import express from "express";
-import type { Router, Request, Response } from "express";
+import type { Router } from "express";
 
-import type { Pet } from "../data/pets";
-
-import { pets } from "../data/pets";
+import { getPetById, getPets } from "../controllers/pets.controllers";
+import { pleaseAuth, validateNumericId } from "../middleware/pets.middleware";
 
 export const petRouter: Router = express.Router();
 
-type PetQueryParams = {
-  species?: string;
-  adopted?: "true" | "false";
-  minAge?: string;
-  maxAge?: string;
-};
+petRouter.get("/", getPets);
 
-petRouter.get(
-  "/",
-  (
-    req: Request<{}, unknown, {}, PetQueryParams>,
-    res: Response<Pet[]>,
-  ): void => {
-    const { species, adopted, minAge, maxAge } = req.query;
-
-    let filteredPets: Pet[] = pets;
-
-    //filter always return array even when empty (not undefined like find)
-    if (species) {
-      filteredPets = filteredPets.filter((pet: Pet): Boolean => {
-        return pet.species.toLowerCase() === species.toLowerCase();
-      });
-    }
-
-    if (adopted) {
-      filteredPets = filteredPets.filter((pet: Pet): Boolean => {
-        return pet.adopted === JSON.parse(adopted);
-      });
-    }
-
-    if (minAge) {
-      filteredPets = filteredPets.filter((pet: Pet): Boolean => {
-        return pet.age >= Number(minAge);
-      });
-    }
-
-    if (maxAge) {
-      filteredPets = filteredPets.filter((pet: Pet): Boolean => {
-        return pet.age <= Number(maxAge);
-      });
-    }
-
-    res.status(200).json(filteredPets);
-  },
-);
-
-petRouter.get(
-  "/pets/:id",
-  (
-    req: Request<{ id: string }>,
-    res: Response<Pet | { statusCode: number; message: string }>,
-  ): void => {
-    const id = req.params.id;
-
-    //find returns undefined when empty
-    const pet: Pet | undefined = pets.find(
-      (pet: Pet): boolean => pet.id.toString() === id,
-    );
-    if (!pet) {
-      res.status(404).json({ statusCode: 404, message: "Pet not found" });
-    }
-
-    res.status(200).json(pet);
-  },
-);
+petRouter.get("/pets/:id", pleaseAuth, validateNumericId, getPetById);
